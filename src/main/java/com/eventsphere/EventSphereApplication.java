c069422d32697b8f0c6ff02404305167f7be05d2
Ajay Gour <ajgour14@gmail.com>
2025-02-18 04:26:33 +0530

    added boiler code

---
+++ src/main/java/com/eventsphere/EventSphereApplication.java
@@ -0,0 +1,14 @@
+package com.eventsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSphereApplication.class, args);
+		System.out.println("============ application Started : Smart EventSphere ============");
+	}
+
+}
---
+++ src/test/java/com/eventsphere/EventSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
